e4a5d69ad2c4a98ab3f07dcb1d1dbc3870ca831d
Antony Gandonou migan <gmigan.a@gmail.com>
2024-08-30 09:12:01 +0200

    feat: the scrapper save followed feed posts into the database

--- scraper.go
+++ scraper.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"github.com/c00rni/rss-go/internal/database"
+	"github.com/google/uuid"
 	"log"
 	"sync"
 	"time"
@@ -20,17 +22,55 @@ func (cfg apiConfig) scrapFeeds(timeBetweenRequest time.Duration, limit int) {
 		}
 
 		for _, feed := range feeds {
-			wg.Add(1)
-			go func(feed database.Feed) {
-				_, err := fetchRSS(feed.Url)
-				if err != nil {
-					log.Printf("Couldn't fetch Feed at %s", feed.Url)
-				} else {
-					log.Printf("Feed %v updated.", feed.Name)
-				}
-				cfg.DB.MarkFeedFetched(context.Background(), feed.ID)
-				defer wg.Done()
-			}(feed)
+			rssFeed, err := fetchRSS(feed.Url)
+			if err != nil {
+				log.Printf("Couldn't fetch Feed at %s", feed.Url)
+				continue
+			}
+			for _, postItem := range rssFeed.Channel.Items {
+				wg.Add(1)
+				go func() {
+
+					postId, err := uuid.NewUUID()
+					if err != nil {
+						log.Printf("Coudn't create post uuid : %v", err)
+						return
+					}
+
+					description := sql.NullString{String: postItem.Description}
+					if postItem.Description != "" {
+						description.Valid = true
+					} else {
+						description.Valid = false
+					}
+
+					today := time.Now()
+
+					publishedTime, err := time.Parse(time.RFC3339, postItem.PubDate)
+					if err != nil {
+						publishedTime = today
+					}
+
+					_, err = cfg.DB.CreatePost(context.Background(), database.CreatePostParams{
+						ID:          postId,
+						CreatedAt:   today,
+						UpdatedAt:   today,
+						Title:       postItem.Title,
+						Url:         postItem.Link,
+						Description: description,
+						PublishedAt: publishedTime,
+						FeedID:      feed.ID,
+					})
+					if err != nil {
+						return
+					}
+
+					log.Printf("[New Post] - Feed %s - Title: %s", feed.Name, postItem.Title)
+					defer wg.Done()
+				}()
+			}
+			cfg.DB.MarkFeedFetched(context.Background(), feed.ID)
+			log.Printf("Feed %v updated.", feed.Name)
 		}
 		wg.Wait()
 	}
